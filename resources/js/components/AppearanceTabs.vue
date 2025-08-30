c16d9237ad8b1a89e9144e3306a28ead1828dfac
jmarie21 <johnmarieygot21@gmail.com>
2025-08-30 16:59:38 +0800

    feat: update the default theme to light and remove dark and system option

--- resources/js/components/AppearanceTabs.vue
+++ resources/js/components/AppearanceTabs.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { useAppearance } from '@/composables/useAppearance';
-import { Monitor, Moon, Sun } from 'lucide-vue-next';
+import { Sun } from 'lucide-vue-next';
 
 const { appearance, updateAppearance } = useAppearance();
 
 const tabs = [
     { value: 'light', Icon: Sun, label: 'Light' },
-    { value: 'dark', Icon: Moon, label: 'Dark' },
-    { value: 'system', Icon: Monitor, label: 'System' },
+    // { value: 'dark', Icon: Moon, label: 'Dark' },
+    // { value: 'system', Icon: Monitor, label: 'System' },
 ] as const;
 </script>
 
