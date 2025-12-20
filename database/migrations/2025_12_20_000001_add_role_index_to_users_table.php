7d567cdfb9ec1024a38dec7d9513277a7b21cb01
John Marie B. Ygot <johnmarieygot21@gmail.com>
2025-12-20 19:41:22 +0800

    feat: Optimize notification and user role queries with new indexes

---
+++ database/migrations/2025_12_20_000001_add_role_index_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * 
+     * This index improves performance of queries filtering by user role
+     * which are used frequently for fetching clients and editors.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->index('role', 'users_role_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropIndex('users_role_index');
+        });
+    }
+};
